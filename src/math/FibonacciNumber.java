2fd087dd6f5bcd166068691d5e286d85bf4a9a6b
Dongye Meng <dmeng@cs.unc.edu>
2014-08-07 22:54:20 -0400

    format

--- src/math/FibonacciNumber.java
+++ src/math/FibonacciNumber.java
@@ -1,7 +1,7 @@
 package math;
 
 /**
- * 1. In the for loop, i starts at 2!!!
+ * 1. In the for loop, i starts at 2!!! 
  * 2. Fib(0) = 0, Fib(1) = 1
  */
 public class FibonacciNumber {
@@ -12,20 +12,20 @@ public class FibonacciNumber {
 		if (n == 0) {
 			return f0;
 		}
-		
+
 		for (int i = 2; i <= n; i++) {
 			fn = f0 + f1;
 			f0 = f1;
 			f1 = fn;
 		}
-		
+
 		return f1;
 	}
-	
+
 	// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
 	public static void main(String[] args) {
 		int n = 10;
 		FibonacciNumber myFib = new FibonacciNumber();
-		System.out.println(myFib.fib(n));		
+		System.out.println(myFib.fib(n));
 	}
 }
