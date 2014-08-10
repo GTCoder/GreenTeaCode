9e458cb8d3c067cca1d10c5bc88e7295cfdc3c1c
Dongye Meng <dmeng@cs.unc.edu>
2014-08-09 23:00:25 -0400

    singleton

---
+++ src/designpattern/Singleton.java
@@ -0,0 +1,32 @@
+package designpattern;
+
+public class Singleton {
+	private static Singleton instance = null;
+	private int data;
+
+	private Singleton(int data) {
+		this.data = data;
+	}
+
+	public static Singleton getInstance(int data) {
+		if (instance == null) {
+			instance = new Singleton(data);
+		}
+
+		return instance;
+	}
+
+	public void printData() {
+		System.out.println("Data: " + this.data);
+	}
+
+	public static void main(String[] args) {
+		Singleton s1 = Singleton.getInstance(0);
+		Singleton s2 = Singleton.getInstance(1);
+		Singleton s3 = Singleton.getInstance(2);
+
+		s1.printData();
+		s2.printData();
+		s3.printData();
+	}
+}
